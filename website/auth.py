610b43fb59b384950381e6ade71a8f0219ef858a
alex <iammalex125@gmail.com>
2026-08-04 00:00:59 +0530

    password validation and flashes in python

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,22 @@
-from flask import Blueprint, render_template,request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth',__name__)
 
 @auth.route('/sign-up', methods = ['POST','GET'])
 def sign_up():
+
+    enter_pswd = request.form.get('password1')
+    reenter_pswd = request.form.get('password2')
+
+    if enter_pswd != reenter_pswd:
+        flash("The two passwords must match", category = "error")
+    else:
+        flash("Account created", category = "success")
+
+    if request.method == "POST":
+        data = request.form
+        print(data)
+
     return render_template("sign-up.html")
 
 @auth.route('/login')
