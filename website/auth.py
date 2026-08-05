9603d2dc835378c5c72c6ebda1be508065ec3da2
alex <iammalex125@gmail.com>
2026-08-06 01:20:02 +0530

    sign-in endpoint created and made changes to sign-up endpoint

--- website/auth.py
+++ website/auth.py
@@ -7,21 +7,23 @@ def sign_up():
 
     enter_pswd = request.form.get('password1')
     reenter_pswd = request.form.get('password2')
+    usernanme = request.form.get('username')  # check if username already exists later on 
+    email = request.form.get('email') # check if user is already authenticated
 
     if enter_pswd != reenter_pswd:
         flash("The two passwords must match", category = "error")
     else:
         flash("Account created", category = "success")
 
-    if request.method == "POST":
-        data = request.form
-        print(data)
 
     return render_template("sign-up.html")
 
-@auth.route('/login')
+@auth.route('/sign-in', methods = ['POST','GET'])
 def login():
-    return "LOGIN"
+    
+    # auth logic goes here 
+
+    return render_template("sign-in.html")
 
 @auth.route('/logout')
 def logout():
